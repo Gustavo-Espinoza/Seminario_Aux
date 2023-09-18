67818df634d7057a371d065387e47915092a2dbc
Gustavo-Espinoza <jamieespinoza93@gmail.com>
2023-09-17 20:04:04 -0400

    Practica_1_P5

--- practica1.js
+++ practica1.js
@@ -1,8 +1,23 @@
-//PREGUNTA 4 ARRAYS AND ARRAYS METHODS
-let transformarATipos=[1,"casa",{}];
-let tiposDeArray=[];
-for(let i=0; i<transformarATipos.length;i++){
-    tiposDeArray.push(typeof(transformarATipos[i]));
-}
-console.log(transformarATipos);
-console.log(tiposDeArray);
+//PREGUNTA 5 DESTRUCTURING
+const person ={
+    head:{
+        eyes: '8',
+        mouth:{
+            teeth: '7',
+            tongue: '4',
+        }
+    },
+    body:{
+        shoulders: '10',
+        chest: '9',
+        arms: '8.5',
+        hands: '9',
+        legs: '10'
+    }
+};
+const myLegs=person.body.legs;
+const myEyes=person.head.eyes;
+document.write(`Result for legs: ${myLegs} <br>`)
+document.write(`Result for eyes: ${myEyes} `)
+console.log(myLegs);
+console.log(myEyes);
