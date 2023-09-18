702f2796ec66d4a484b9e3a92aae95ceb27fbd2a
Gustavo-Espinoza <jamieespinoza93@gmail.com>
2023-09-17 20:07:59 -0400

    Practica_1_P6

--- practica1.js
+++ practica1.js
@@ -1,23 +1,10 @@
-//PREGUNTA 5 DESTRUCTURING
-const person ={
-    head:{
-        eyes: '8',
-        mouth:{
-            teeth: '7',
-            tongue: '4',
-        }
-    },
-    body:{
-        shoulders: '10',
-        chest: '9',
-        arms: '8.5',
-        hands: '9',
-        legs: '10'
-    }
-};
-const myLegs=person.body.legs;
-const myEyes=person.head.eyes;
-document.write(`Result for legs: ${myLegs} <br>`)
-document.write(`Result for eyes: ${myEyes} `)
-console.log(myLegs);
-console.log(myEyes);
+//PREGUNTA 6 STRING TEMPLATE
+const name="Gustavo";
+const firstName="Jamie";
+const lastName="Espinoza";
+const age="22";
+const hubby="jugar juegos";
+
+let frase=`La frase generada es: <b>Yo</b> ${name} ${firstName}
+ ${lastName} con edad ${age} tengo el hobby de ${hubby}`
+document.write(frase)
